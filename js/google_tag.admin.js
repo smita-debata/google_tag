7cdd847bb900095bd7bdded1f5ab68c242f88402
solotandem <jim@boombatower.com>
2016-12-30 17:59:55 -0600

    Update admin.js to 8.x core API.
      Replace 'fieldset' with 'details' in find expressions.
      Remove 'google_tag_' prefix from setting names.

--- js/google_tag.admin.js
+++ js/google_tag.admin.js
@@ -7,80 +7,78 @@
 
 (function ($) {
 
-/**
- * Provides summary information for the vertical tabs.
- */
-Drupal.behaviors.gtmInsertionSettings = {
-  attach: function (context) {
-    if (typeof jQuery.fn.drupalSetSummary == 'undefined') {
-      // This behavior only applies if drupalSetSummary is defined.
-      return;
-    }
+  "use strict";
 
-    $('fieldset#edit-path', context).drupalSetSummary(function (context) {
-      var $radio = $('input[name="google_tag_path_toggle"]:checked', context);
-      if ($radio.val() == 'exclude listed') {
-        if (!$('textarea[name="google_tag_path_list"]', context).val()) {
-          return Drupal.t('All paths');
+  /**
+  * Provides summary information for the vertical tabs.
+  */
+  Drupal.behaviors.gtmInsertionSettings = {
+    attach: function (context) {
+
+      $('details#edit-path', context).drupalSetSummary(function (context) {
+        var $radio = $('input[name="path_toggle"]:checked', context);
+        if ($radio.val() == 'exclude listed') {
+          if (!$('textarea[name="path_list"]', context).val()) {
+            return Drupal.t('All paths');
+          }
+          else {
+            return Drupal.t('All paths except listed paths');
+          }
         }
         else {
-          return Drupal.t('All paths except listed paths');
+          if (!$('textarea[name="path_list"]', context).val()) {
+            return Drupal.t('No paths');
+          }
+          else {
+            return Drupal.t('Only listed paths');
+          }
         }
-      }
-      else {
-        if (!$('textarea[name="google_tag_path_list"]', context).val()) {
-          return Drupal.t('No paths');
-        }
-        else {
-          return Drupal.t('Only listed paths');
-        }
-      }
-    });
-
-    $('fieldset#edit-role', context).drupalSetSummary(function (context) {
-      var vals = [];
-      $('input[type="checkbox"]:checked', context).each(function () {
-        vals.push($.trim($(this).next('label').text()));
       });
-      var $radio = $('input[name="google_tag_role_toggle"]:checked', context);
-      if ($radio.val() == 'exclude listed') {
-        if (!vals.length) {
-          return Drupal.t('All roles');
-        }
-        else {
-          return Drupal.t('All roles except selected roles');
-        }
-      }
-      else {
-        if (!vals.length) {
-          return Drupal.t('No roles');
-        }
-        else {
-          return Drupal.t('Only selected roles');
-        }
-      }
-    });
 
-    $('fieldset#edit-status', context).drupalSetSummary(function (context) {
-      var $radio = $('input[name="google_tag_status_toggle"]:checked', context);
-      if ($radio.val() == 'exclude listed') {
-        if (!$('textarea[name="google_tag_status_list"]', context).val()) {
-          return Drupal.t('All statuses');
+      $('details#edit-role', context).drupalSetSummary(function (context) {
+        var vals = [];
+        $('input[type="checkbox"]:checked', context).each(function () {
+          vals.push($.trim($(this).next('label').text()));
+        });
+        var $radio = $('input[name="role_toggle"]:checked', context);
+        if ($radio.val() == 'exclude listed') {
+          if (!vals.length) {
+            return Drupal.t('All roles');
+          }
+          else {
+            return Drupal.t('All roles except selected roles');
+          }
         }
         else {
-          return Drupal.t('All statuses except listed statuses');
+          if (!vals.length) {
+            return Drupal.t('No roles');
+          }
+          else {
+            return Drupal.t('Only selected roles');
+          }
         }
-      }
-      else {
-        if (!$('textarea[name="google_tag_status_list"]', context).val()) {
-          return Drupal.t('No statuses');
+      });
+
+      $('details#edit-status', context).drupalSetSummary(function (context) {
+        var $radio = $('input[name="status_toggle"]:checked', context);
+        if ($radio.val() == 'exclude listed') {
+          if (!$('textarea[name="status_list"]', context).val()) {
+            return Drupal.t('All statuses');
+          }
+          else {
+            return Drupal.t('All statuses except listed statuses');
+          }
         }
         else {
-          return Drupal.t('Only listed statuses');
+          if (!$('textarea[name="status_list"]', context).val()) {
+            return Drupal.t('No statuses');
+          }
+          else {
+            return Drupal.t('Only listed statuses');
+          }
         }
-      }
-    });
-  }
-};
+      });
+    }
+  };
 
 })(jQuery);
